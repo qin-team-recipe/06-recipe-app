eea40c66d2d1a5a6470cafa5a1305fa48b91ec64
XNCOAISN <end.pgm@gmail.com>
2023-06-08 02:26:39 +0900

    feat: :sparkles: usage example for prisma with route handler

--- app/api/chefs/route.ts
+++ app/api/chefs/route.ts
@@ -1,10 +1,8 @@
-import { LOCAL_STORAGE_KEYS } from '@/_common/_constants/localStorage';
-import { NextResponse, type NextRequest } from 'next/server';
+import { prisma } from 'libs/prisma';
+import { NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest): Promise<NextResponse> {
-  const res = NextResponse.json({ name: 'taro' });
-
-  res.cookies.set(LOCAL_STORAGE_KEYS.NICKNAME, 'john');
-
-  return res;
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export async function GET() {
+  const chefs = await prisma.chef.findMany();
+  return NextResponse.json({ chefs });
 }
