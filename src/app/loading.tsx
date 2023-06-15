54d97660478781c88f652624b1df0b3cbcdd0145
NSJ <nsjkun@gmail.com>
2023-06-16 03:10:02 +0900

    feat: 🎨 redesign the loading screen

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,11 @@
+// ローディング
 const Loading = () => {
-  return <div>ローディング中。。</div>;
-};
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="p-6">ローディング中...</div>
+      <div className="h-10 w-10 animate-spin rounded-full border-4 border-indigo-300 border-t-transparent" />
+    </div>
+  )
+}
 
-export default Loading;
+export default Loading
