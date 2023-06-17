f3f306fb3a84631065c13866287042c7ca1070ad
yanachuwan9sm <gurem919@icloud.com>
2023-06-17 13:43:14 +0900

    style: delete comment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-// const { blue, mauve, mauveA, tomato } = require('@radix-ui/colors');
-// const colors = require('tailwindcss/colors');
-
-// const customColors = {
-//   primary: {
-//     DEFAULT: tomato.tomato9,
-//     foreground: tomato.tomato11,
-//   },
-//   secondary: blue.blue11,
-//   background: colors.white,
-//   foreground: mauve.mauve12,
-//   border: mauve.mauve7,
-//   destructive: {
-//     DEFAULT: mauve.mauve8,
-//     foreground: mauve.mauve9,
-//   },
-//   surface: {
-//     primary: mauve.mauve1,
-//     secondary: mauve.mauve4,
-//     tertiary: mauve.mauve6,
-//   },
-//   grey: {
-//     DEFAULT: mauve.mauve10,
-//     foreground: mauve.mauve11,
-//     alpha: mauveA.mauveA10,
-//   },
-// };
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
