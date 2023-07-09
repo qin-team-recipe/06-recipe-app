2c0ca7946e3a9487d2859fbe7cf5262316d4406a
kakizawaayato <chibairodori25@gmail.com>
2023-07-09 16:30:55 +0900

    refactor: 🔄 Code modifications for improved readability（#61）

--- src/app/(app)/recipe/[recipeId]/MyRecipeVisibilityBadge/MyRecipeVisibilityBadge.tsx
+++ src/app/(app)/recipe/[recipeId]/MyRecipeVisibilityBadge/MyRecipeVisibilityBadge.tsx
@@ -7,18 +7,15 @@ export type MyRecipeVisibilityBadgeProps = {
 const myRecipeVisibilityBadge = tv({
   base: 'rounded-2 text-orange-300 w-32 rounded border px-1 py-1 text-center text-xs',
   variants: {
-    publishingState: {
-      public: 'border-tomato-8 bg-tomato-2 text-tomato-11',
-      private: 'border-mauve-8 bg-mauve-2 text-mauve-11',
+    isPublic: {
+      true: 'border-tomato-8 bg-tomato-2 text-tomato-11',
+      false: 'border-mauve-8 bg-mauve-2 text-mauve-11',
     },
   },
 });
 
 export const MyRecipeVisibilityBadge = ({ isPublic }: MyRecipeVisibilityBadgeProps) => {
   const text = isPublic ? '公開中のマイレシピ' : '非公開のマイレシピ';
-  const publishingState = isPublic ? 'public' : 'private';
 
-  return (
-    <div className={myRecipeVisibilityBadge({ publishingState: publishingState })}>{text}</div>
-  );
+  return <div className={myRecipeVisibilityBadge({ isPublic: isPublic })}>{text}</div>;
 };
