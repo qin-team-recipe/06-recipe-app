24270a22abf3617b2017b6b0e2b156df0f809083
ya7gi <95360878+yanachuwan9sm@users.noreply.github.com>
2023-07-21 22:09:02 +0900

    feat: :sparkles: add recipeList Components and Story. (#76)
    close #56

--- src/app/(app)/_components/RecipeList/index.tsx
+++ src/app/(app)/_components/RecipeList/index.tsx
@@ -1,50 +1,46 @@
+import { Recipe } from '@/types';
+import { HorizontalScrollArea } from '../HorizontalScrollArea';
 import { RecipeListItem, RecipeListItemVariant } from '../RecipeListItem';
 
 type RecipeListProps = {
-  size?: RecipeListItemVariant['size'];
+  recipeData: Recipe[];
+  direction?: RecipeListItemVariant['direction'];
 };
 
-// TODO:ダミーデータ & プロパティも適当なので削除する
-const dummyRecipeData = [
-  {
-    id: '1',
-    name: '定番レシピA',
-    description: '短い説明文',
-    favorite: 100,
-  },
-  {
-    id: '2',
-    name: '定番レシピB',
-    description: '少し長めの説明文',
-    favorite: 0,
-  },
-  {
-    id: '3',
-    name: '少しタイトルが長めの定番レシピC',
-    description: '少し長めの説明文少し長めの説明文少し長めの説明文',
-    favorite: 36,
-  },
-  {
-    id: '4',
-    name: 'タイトルがそこそこ長めの大人気定番レシピC',
-    description: '少し長めの説明文少し長めの説明文少し長めの説明文',
-    favorite: 200,
-  },
-];
+export const RecipeList = ({ direction = 'horizontal', recipeData }: RecipeListProps) => {
+  if (direction === 'vertical') {
+    return (
+      <div className="grid grid-cols-2 gap-4 px-4">
+        {recipeData.map(({ id, name, description, favorite }) => (
+          <RecipeListItem
+            direction="vertical"
+            key={id}
+            id={id}
+            name={name}
+            description={description}
+            favorite={favorite}
+          />
+        ))}
+      </div>
+    );
+  }
 
-export const RecipeList = ({ size }: RecipeListProps) => {
   return (
     <>
-      {dummyRecipeData.map(({ id, name, description, favorite }) => (
-        <RecipeListItem
-          size={size}
-          key={id}
-          id={id}
-          name={name}
-          description={description}
-          favorite={favorite}
-        />
-      ))}
+      <HorizontalScrollArea>
+        <div className="flex flex-row gap-x-4 px-4">
+          {recipeData.map(({ id, name, description, favorite }) => (
+            <RecipeListItem
+              direction="horizontal"
+              key={id}
+              id={id}
+              name={name}
+              description={description}
+              favorite={favorite}
+            />
+          ))}
+        </div>
+      </HorizontalScrollArea>
     </>
   );
 };
