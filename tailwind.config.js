d281c0f73549620c910879e019a106045868f313
yanachuwan9sm <gurem919@icloud.com>
2023-07-21 16:44:22 +0900

    style: :memo: add box-shadow variations.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     extend: {
       boxShadow: {
         base: '0px -2px 4px rgba(0, 0, 0, 0.12)',
+        smallInner: '0px 0px 4px 0px hsla(0, 0%, 0%, 0.5) inset',
         inner: '0px -60px 60px 0px hsla(0, 0%, 0%, 0.5) inset',
+        heroInner: '0px -60px 16px -40px #FFF inset',
       },
     },
   },
