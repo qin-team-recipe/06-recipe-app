a0566e388c38e8b2addb636b38176f2c9f65199e
yanachuwan9sm <gurem919@icloud.com>
2023-07-26 19:23:38 +0900

    style: :truck: 不要なコメントアウトを削除

--- src/app/(app)/_hooks/useSearchDebounce.ts
+++ src/app/(app)/_hooks/useSearchDebounce.ts
@@ -1,33 +1,3 @@
-// import { useEffect, useState } from 'react';
-
-// type UseSearchDebounceHook<T> = [T, boolean];
-
-// export function useSearchDebounce<T>(
-//   value: T,
-//   delay?: number,
-//   callBackFn?: () => void
-// ): UseSearchDebounceHook<T> {
-//   const [debouncedValue, setDebouncedValue] = useState<T>(value);
-//   const [isLoading, setIsLoading] = useState<boolean>(false);
-
-//   useEffect(() => {
-//     if (value) setIsLoading(true);
-
-//     const timer = setTimeout(() => {
-//       setDebouncedValue(value);
-//       callBackFn && callBackFn();
-//       setIsLoading(false);
-//     }, delay || 500);
-
-//     return () => {
-//       clearTimeout(timer);
-//     };
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, [value, delay]);
-
-//   return [debouncedValue, isLoading];
-// }
-
 import { useCallback, useRef } from 'react';
 
 type Debounce = (fn: () => void) => void;
