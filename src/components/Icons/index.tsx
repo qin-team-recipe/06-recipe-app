68dec8f2bd5cffa456253ebe9e6fb84d3a54b67e
yanachuwan9sm <gurem919@icloud.com>
2023-07-26 17:03:35 +0900

    fix: :bug: fix to review comments.

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,5 +1,5 @@
 import { IconArrowLeft, IconSearch, IconToolsKitchen2 } from '@tabler/icons-react';
-import { ChefHat, Heart, LucideProps, Settings, ShoppingCart } from 'lucide-react';
+import { ChefHat, Heart, Settings, ShoppingCart } from 'lucide-react';
 
 export const Icons = {
   Search: IconSearch,
@@ -9,10 +9,4 @@ export const Icons = {
   Setting: Settings,
   ToolsKitchen2: IconToolsKitchen2,
   BackArrow: IconArrowLeft,
-  HeroBackArrow: (props: LucideProps) => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="none" {...props}>
-      <path d="M25.3334 16H6.66669" stroke-linecap="round" stroke-linejoin="round" />
-      <path d="M16 25.3333L6.66669 16L16 6.66666" stroke-linecap="round" stroke-linejoin="round" />
-    </svg>
-  ),
 };
